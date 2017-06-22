aa3a17bdc2d30a3bff1997cce137f1aec148686c
Devinder Khroad <dkhroad@gmail.com>
2017-06-22 15:26:16 -0700

    first web server implementation!

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
   "fmt"
+  "net/http"
 )
 
-func main() {
-  fmt.Println("Hello World!") 
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w,"<h1>Welcome to my awesome site!</h1>")
 }
+
+func main() {
+  http.HandleFunc("/",handlerFunc)
+  http.ListenAndServe(":3000",nil /* use builtin server mux */)
+}
+
