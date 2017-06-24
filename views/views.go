4644c059dbb2a16e1ea395c058806803f6761974
Devinder Khroad <dkhroad@gmail.com>
2017-06-23 21:19:59 -0700

    Cleanup view code a bit

--- views/views.go
+++ views/views.go
@@ -1,13 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   = "views/layouts"
+	TemplateExt = "*.tmpl"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.tmpl",
-		"views/layouts/bootstrap.tmpl",
-		"views/layouts/navbar.tmpl",
-	)
+	files = append(files, layoutFiles()...)
+
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -21,4 +26,12 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(filepath.Join(LayoutDir, TemplateExt))
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
