82019ffdede3736989727c863c3ec01dd48fe00b
Devinder Khroad <dkhroad@gmail.com>
2017-06-26 11:46:51 -0700

    Added users controller

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 	"net/http"
 )
@@ -9,7 +10,6 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -22,20 +22,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.tmpl")
 	contactView = views.NewView("bootstrap", "views/contact.tmpl")
-	signupView = views.NewView("bootstrap", "views/signup.tmpl")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	http.ListenAndServe(":3000", r)
 }
 
